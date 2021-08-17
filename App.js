0fe59cf22e9eeac8aa00fbb19387dbafd2b09f14
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-17 11:36:44 +0700

    Refactor

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
+import { AntDesign, Feather, MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet } from "react-native";
 import Detail from "./Screens/Detail";
 import Home from "./Screens/Home";
 import Settings from "./Screens/Settings";
@@ -11,7 +11,20 @@ const Stack = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            if (route.name === "Home") {
+              return <AntDesign name="home" size={24} color="black" />;
+            } else if (route.name === "Settings") {
+              return <Feather name="settings" size={24} color="black" />;
+            } else if (route.name === "Detail") {
+              return <MaterialIcons name="details" size={24} color="black" />;
+            }
+          },
+        })}
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen name="Detail" component={Detail} />
@@ -19,13 +32,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings Screen</Text>
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+        }}
+      >
+        House Listing App
+      </Text>
+      <Text>Version 0.1</Text>
+      <Text>Author: Vinh Hoang</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default Settings;
